5767fa112229dc18b2f757843275a6b7909c97df
shahnozahaydarova <shahnozahaydarova03@gmail.com>
2022-12-13 23:46:20 +0500

    block content and include

--- backend/urls.py
+++ backend/urls.py
@@ -2,18 +2,18 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('',HomeView.as_view()),
-    path('about/',AboutView.as_view()),
-    path('author/',AuthorView.as_view()),
-    path('contact/',ContactView.as_view()),
-    path('indexfullleft/',IndexFullLeftView.as_view()),
-    path('indexfullright/',IndexFullRightView.as_view()),
-    path('indexfull/',IndexFullView.as_view()),
-    path('indexlistleft/',IndexListLeftView.as_view()),
-    path('indexlistright/',IndexListRightView.as_view()),
-    path('indexlist/',IndexListView.as_view()),
-    path('postdetails1/',PostDetails1View.as_view()),
-    path('postdetails2/',PostDetails2View.as_view()),
-    path('postelements/',PostElementsView.as_view()),
-    path('privacypolicy/',PrivacyPolicyView.as_view()),
-    path('termsconditions/',TermsConditionsView.as_view())
+    path('about/',AboutView.as_view(), name="about" ),
+    path('author/',AuthorView.as_view(),name="author" ),
+    path('contact/',ContactView.as_view() , name= "contact" ),
+    path('indexfullleft/',IndexFullLeftView.as_view(),name="ifl" ),
+    path('indexfullright/',IndexFullRightView.as_view() , name="ifr" ),
+    path('indexfull/',IndexFullView.as_view() , name="if" ),
+    path('indexlistleft/',IndexListLeftView.as_view() , name="ill" ),
+    path('indexlistright/',IndexListRightView.as_view() , name="ilr"),
+    path('indexlist/',IndexListView.as_view() , name="il"),
+    path('postdetails1/',PostDetails1View.as_view(),name="pd1" ),
+    path('postdetails2/',PostDetails2View.as_view() , name="pd2" ),
+    path('postelements/',PostElementsView.as_view() , name="pe" ),
+    path('privacypolicy/',PrivacyPolicyView.as_view() , name="pp"),
+    path('termsconditions/',TermsConditionsView.as_view(), name="tc")
 ]
